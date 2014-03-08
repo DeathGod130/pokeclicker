fddb1ba1f1eb4073ad132e047f87422f10b56fa9
Jack Miesse <poseidongraal@gmail.com>
2014-03-08 15:42:38 -0800

    Dem Moneys

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 var pokemonElement = document.getElementById("pokemon")
 var pokemon = 0;
+var money = 0;
+var sell = document.getElementById("sell")
+var moneyElement = document.getElementById("money")
 var pokeClicks = document.getElementById("pokeclicks")
 
 pokeClicks.addEventListener("click", function() {
   catchPokemon(1);
 }, false);
 
+sell.addEventListener("click", function() {
+  sellPokemon(5);
+}, false);
+
 function catchPokemon(numbercaught) {
   pokemon += numbercaught;
   pokemonElement.innerHTML = pokemon;
+}
+
+function sellPokemon(moolah) {
+  pokemon --;
+  pokemonElement.innerHTML = pokemon;
+  money += moolah;
+  moneyElement.innerHTML = money;
 }
